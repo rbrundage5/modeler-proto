1fa55c6b49e811a417a5082e95f616f2348002d8
rbrundage5 <rbrundage5@gmail.com>
2026-08-14 10:23:42 -0400

    Add incremental change-set coordinator

---
+++ public/src/incremental-change-set.js
@@ -0,0 +1,11 @@
+import {dependencyClosure,descendantClosure} from './incremental-dependencies.js';
+import {markElementDirty,markRelationshipDirty,markDiagramDirty} from './dirty-set.js';
+
+export function recordSemanticChange(project,{elementIds=[],relationshipIds=[],diagramIds=[],ownerIds=[],includeDescendants=false}={}){
+  const seedElements=includeDescendants?[...descendantClosure(project,elementIds)]:elementIds;
+  const closure=dependencyClosure(project,{elementIds:seedElements,relationshipIds,diagramIds,ownerIds});
+  for(const id of closure.elements)markElementDirty(project,id);
+  for(const id of closure.relationships)markRelationshipDirty(project,id);
+  for(const id of closure.diagrams)markDiagramDirty(project,id);
+  return closure;
+}
